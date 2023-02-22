05e5f38d9bf2285d81893b07c8cf569b4cfdf3d9
David Isidore <isidoredavid@protonmail.com>
2023-02-22 13:30:12 +0100

    Implémentation de la barre de navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
+import Navbar from '../Navbar/Navbar'
 
 export default function Header() {
     return (
         <div>
             <img src={logo} alt="logo de Booki" />
+            <Navbar />
         </div>
     )
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <ul>
+            <li>Hébergements</li>
+            <li>Activités</li>
+        </ul>
+    )
+}
