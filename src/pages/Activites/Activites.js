dd78ef61ba922e07c0f02c8242291cdf2811b304
David Isidore <isidoredavid@protonmail.com>
2023-02-22 16:32:58 +0100

    Modification de l'affichage du nom des pages Hébergements et Activités

--- src/pages/Activites/Activites.js
+++ src/pages/Activites/Activites.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 export default function Activites() {
+    useEffect(() => {
+        document.title = 'Activités'
+    })
     return (
         <div>
             <h1>Activités</h1>
