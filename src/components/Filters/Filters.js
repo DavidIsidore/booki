af534e15b94b0d4cc886dd625bb8f87059351e1b
David Isidore <isidoredavid@protonmail.com>
2023-02-23 20:49:45 +0100

    Mise en place des fonts des filtres

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,3 +1,10 @@
+import {
+    faChildReaching,
+    faMoneyBill1Wave,
+    faHeart,
+    faDog,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import './filters.scss'
 
@@ -6,7 +13,25 @@ export default function Filters() {
         <div className="filters">
             <div>
                 <h3>Filtres</h3>
-                <div>{/*ici lesfiltres*/}</div>
+                <div>
+                    {/*ici lesfiltres*/}
+                    <div>
+                        <FontAwesomeIcon icon={faMoneyBill1Wave} />
+                        <span>Économique</span>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faChildReaching} />
+                        <span>Familial</span>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faHeart} />
+                        <span>Romantique</span>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faDog} />
+                        <span>Animaux autorisés</span>
+                    </div>
+                </div>
             </div>
             <div className="filters_info">
                 <div></div>
