8025a034db19637ff6c23e0c531dfeab27f11ebd
David Isidore <isidoredavid@protonmail.com>
2023-02-24 12:05:20 +0100

    Mise en place d'un exemple d'appartement

---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+const datas = [
+    {
+        id: 'abcdef',
+        title: 'Hôtel chez Amina',
+        price: 'Nuit à partir de 96 €',
+        picture: '../../assets/hebergements/amina.jpg',
+        rating: '5',
+        tags: 'animaux',
+    },
+]
+
+export default datas
